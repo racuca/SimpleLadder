0e709aa68e65b4adb22543f341e68f52dd75a044
Jason, Lee <racuca@gmail.com>
2026-02-02 16:50:59 +0900

    CompileSdk 36 으로 변경 앱 이름 한국어 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mvpcoding.simpleladder"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mvpcoding.simpleladder"
